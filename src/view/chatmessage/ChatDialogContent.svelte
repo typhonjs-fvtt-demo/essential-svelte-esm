d9881e89f6af6e59a0aec1b6513ea08238cefd6d
typhonrt <support@typhonjs.io>
2022-03-20 10:28:35 -0700

    Simplify dialog content

--- src/view/chatmessage/ChatDialogContent.svelte
+++ src/view/chatmessage/ChatDialogContent.svelte
@@ -6,15 +6,5 @@
    }
 </script>
 
-<main>
-   Creates a custom chat message with flag data that is used to mount `MyChatMessage` Svelte component.
-   <button on:click={onClick}>Post Custom Chat Message</button>
-</main>
-
-<style lang="scss">
-   main {
-      text-align: center;
-      display: flex;
-      flex-direction: column;
-   }
-</style>
+Creates a custom chat message with flag data that is used to mount `MyChatMessage` Svelte component.
+<button on:click={onClick}>Post Custom Chat Message</button>
