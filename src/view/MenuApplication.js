f30a18010947ff7309669bef181b6d82d44f76b8
typhonrt <support@typhonjs.io>
2022-04-16 00:10:57 -0700

    Change position-app import

--- src/view/MenuApplication.js
+++ src/view/MenuApplication.js
@@ -7,7 +7,7 @@ import MenuAppShell              from './MenuAppShell.svelte';
 import ChatDialogContent         from './chatmessage/ChatDialogContent.svelte';
 import HeaderButtonsApplication  from './header-buttons/HeaderButtonsApplication.js';
 import HelloFoundryApplication   from './hello/HelloFoundryApplication.js';
-import PositionApplication       from './position/PositionApplication.js';
+import PositionApplication       from './position-app/PositionApplication.js';
 import PositionBoxApplication    from './position-box/PositionBoxApplication.js';
 import PositionCarouselApp       from './position-carousel/PositionCarouselApp.js';
 
@@ -43,7 +43,6 @@ export default class MenuApplication extends SvelteApplication
             props: {
                buttons: [
                   { title: 'Hello Foundry', class: HelloFoundryApplication },
-                  { title: 'Position', class: PositionApplication },
                   { title: 'Header Buttons', class: HeaderButtonsApplication },
                   { title: 'Chat Message', onclick: () => new TJSDialog(
                      {
@@ -51,8 +50,9 @@ export default class MenuApplication extends SvelteApplication
                         content: ChatDialogContent
                      }, { id: 'essential-esm-chat-dialog' })
                   },
+                  { title: 'Position (App)', class: PositionApplication },
                   { title: 'Position (Box)', class: PositionBoxApplication },
-                  { title: 'Position (Carousel)', class: PositionCarouselApp },
+                  { title: 'Position (Carousel)', class: PositionCarouselApp }
                ]
             }
          }
