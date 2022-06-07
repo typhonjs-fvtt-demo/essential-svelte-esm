8373f1a8a1da48f97c73a7e335c9ea85501f4d1e
typhonrt <support@typhonjs.io>
2022-06-07 01:17:02 -0700

    comments

--- src/view/position-box/_control/control/draggable.js
+++ src/view/position-box/_control/control/draggable.js
@@ -1,8 +1,8 @@
 import { controls } from '../layer/ControlsStore.js';
 
 /**
- * Provides an action to enable pointer dragging of an HTMLElement and invoke `position.set` on a given {@link Position}
- * instance provided. When the attached boolean store state changes the draggable action is enabled or disabled.
+ * Provides an action to enable pointer dragging that tracks relative changes. Sends data to
+ * {@link ControlsStore.dragging}.
  *
  * @param {HTMLElement}       node - The node associated with the action.
  *
