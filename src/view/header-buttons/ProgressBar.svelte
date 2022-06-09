87097a5ff717d4c5e3d55d0f9469826d5130f1cb
typhonrt <support@typhonjs.io>
2022-06-09 14:34:49 -0700

    Update for TRL draggable changes

--- src/view/header-buttons/ProgressBar.svelte
+++ src/view/header-buttons/ProgressBar.svelte
@@ -1,8 +1,12 @@
-<progress class=keep-minimized max=100></progress>
+<!-- Add null mousedown / dblclick event handler to capture before header drag / minimize takes -->
+<progress class=keep-minimized max=100
+          on:mousedown|capture|preventDefault|stopPropagation={() => null}
+          on:dblclick|capture|preventDefault|stopPropagation={() => null} />
 
 <style>
    progress {
       height: 30px;
       margin: 0 0 0 8px;
+      cursor: default;
    }
 </style>
--- src/view/header-buttons/TestSCComponent.svelte
+++ src/view/header-buttons/TestSCComponent.svelte
@@ -1,8 +1,13 @@
-<span>SC</span>
+<!-- Add null mousedown / dblclick event handler to capture before header drag / minimize takes -->
+<span on:mousedown|capture|preventDefault|stopPropagation={() => null}
+      on:dblclick|capture|preventDefault|stopPropagation={() => null}>
+   SC
+</span>
 
 <style>
    span {
       flex: none;
       margin: 0 0 0 8px;
+      cursor: default;
    }
 </style>
