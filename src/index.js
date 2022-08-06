656d27b4dc9c49958dfcbc5034eeee330fbc86e5
typhonrt <support@typhonjs.io>
2022-08-06 13:23:00 -0700

    Improve chat message demo documentation and hooks.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import MenuApplication from './view/MenuApplication.js';
-import MyChatMessage   from './view/chatmessage/MyChatMessage.svelte';
 
+import './chatmessage.js';    // Loads the hooks specific to the chat message demo.
+
+/**
+ * Launches and positions the main `essential-svelte-esm` menu app to the left of the sidebar.
+ */
 Hooks.once('ready', () =>
 {
    const sidebarRect = document.querySelector('#sidebar').getBoundingClientRect();
    new MenuApplication({ left: sidebarRect.x - 210, top: sidebarRect.y }).render(true, { focus: true });
-});
-
-/**
- * Used by chat message demo to attach a Svelte component, MyChatMessage, to a chat message.
- */
-Hooks.on('renderChatMessage', (message, html) =>
-{
-   const flagData = message.getFlag('essential-svelte-esm', 'data');
-
-   if (typeof flagData === 'object')
-   {
-      new MyChatMessage({ target: html[0], props: flagData });
-   }
 });
