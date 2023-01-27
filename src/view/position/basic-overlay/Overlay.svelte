12aa0426398a9ecb1e20a31a7a636364f3becb2e
typhonrt <support@typhonjs.io>
2023-01-26 21:21:49 -0800

    comments / hasTargetClassList example.

--- src/view/position/basic-overlay/Overlay.svelte
+++ src/view/position/basic-overlay/Overlay.svelte
@@ -16,18 +16,35 @@
 <svelte:options accessors={true}/>
 
 <EmptyApplicationShell bind:elementRoot>
-   <!-- width & height will be what you set in app options -->
-   <div use:draggable={{position}}
+   <!--
+    * width & height will be what you set in app options.
+    *
+    * You are in control of adding the draggable action to whatever element that you want to be the drag handle.
+    * In this case it is added to the entire `shell-target` content div therefore to allow focusable elements like the
+    * included input element you should limit draggable targets by `hasTargetClassList`.
+    *
+    * You may choose to have a specific drag handle element that is smaller than the content area and can apply the
+    * draggable action to that without the need for `hasTargetClassList`.
+   -->
+   <div class='shell-target' use:draggable={{ position, hasTargetClassList: ['shell-target'] }}
         on:contextmenu={() => context.application.close()}>
       Context click to close
+      <input type=text placeholder="An input showing focus control" />
    </div>
 </EmptyApplicationShell>
 
 <style>
    div {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
       height: 100%;
       width: 100%;
       background: rgba(255, 100, 100, 0.5);
-      font-size: 2em;
+      padding: 8px;
+   }
+
+   input {
+      background: rgba(255, 0, 0, 0.5)
    }
 </style>
