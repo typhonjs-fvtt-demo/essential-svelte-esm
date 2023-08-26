e5607b1b5d21b893a39c7bd5d235d575043e156c
typhonrt <support@typhonrt.org>
2023-08-27 10:55:04 +1200

    Added comments

--- src/view/hello/HelloFoundryApplication.js
+++ src/view/hello/HelloFoundryApplication.js
@@ -20,6 +20,10 @@ export default class HelloFoundryApplication extends SvelteApplication
          width: 500,
          height: 320,
          title: 'Essential Svelte (ESM) - Hello Foundry',
+
+         // Note: Setting the following options disables the optimized position system and removes the default transform
+         // origin allowing the Svelte transition assigned in `HelloFoundryAppShell.svelte` to control the intro / outro
+         // transitions of the app window. This is only useful to enable the Svelte transition system.
          positionOrtho: false,
          transformOrigin: null,
 
@@ -28,7 +32,7 @@ export default class HelloFoundryApplication extends SvelteApplication
             target: document.body,
             intro: true,
             props: {
-               message: 'Foundry'
+               message: 'Foundry'   // A prop passed to HelloFoundryAppShell for the initial message displayed.
             }
          }
       });
