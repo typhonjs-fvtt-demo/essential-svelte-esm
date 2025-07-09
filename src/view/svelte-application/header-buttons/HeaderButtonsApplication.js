aa54662c2032129186491eaed3405732d820fe03
typhonrt <support@typhonjs.io>
2025-07-08 19:01:09 -0700

    Comments.

--- src/view/svelte-application/header-buttons/HeaderButtonsApplication.js
+++ src/view/svelte-application/header-buttons/HeaderButtonsApplication.js
@@ -23,7 +23,7 @@ export class HeaderButtonsApplication extends SvelteApp
          resizable: true,
          minimizable: true,
          title: 'Essential Svelte (ESM) - Header Buttons',
-         headerIcon: 'icons/magic/air/air-burst-spiral-blue-gray.webp',
+         headerIcon: 'icons/magic/air/air-burst-spiral-blue-gray.webp', // Supports common image formats and SVG.
          // headerIcon: 'fas fa-cogs',    // You may also use a Font Awesome icon.
          width: 600,
          height: 200,
