74b623e71d3578f5166a60d9718b265af2db2553
typhonrt <support@typhonjs.io>
2025-08-16 16:12:11 -0700

    comments.

--- src/view/MenuAppSection.svelte
+++ src/view/MenuAppSection.svelte
@@ -65,7 +65,8 @@
 
 <section>
    <TJSSvgFolder {folder}>
-      <div>
+      <!-- Note the trick of applying `themed theme-dark` to the div to force buttons to core dark theme -->
+      <div class="themed theme-dark">
          {#each section.entries as entry}
             <button on:click={() => onClick(entry)}>{entry.title}</button>
          {/each}
@@ -74,25 +75,6 @@
 </section>
 
 <style lang=scss>
-   // Due to how the theming system works one must explicitly provide a static theme for elements like `button` that
-   // are affected by global theming changes. The styles below hard lock `button` to the default dark theme.
-   button {
-      background-color: var(--color-cool-5-50);
-      border-color: var(--color-light-5);
-      color: var(--color-light-3);
-   }
-
-   button:hover {
-      background: var(--color-warm-2);
-      color: var(--color-light-1);
-      border-color: var(--color-light-3);
-   }
-
-   button:focus {
-      outline: 1px solid var(--color-warm-2);
-      box-shadow: 0 0 4px var(--color-warm-2);
-   }
-
    div {
       display: flex;
       flex-direction: column;
