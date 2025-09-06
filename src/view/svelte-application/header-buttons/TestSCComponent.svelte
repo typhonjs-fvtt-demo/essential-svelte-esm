9945b89c025d38c450ba3ea6d14a6de8b6e338a5
typhonrt <support@typhonjs.io>
2025-09-05 21:50:54 -0700

    Add tooltip.

--- src/view/svelte-application/header-buttons/TestSCComponent.svelte
+++ src/view/svelte-application/header-buttons/TestSCComponent.svelte
@@ -1,9 +1,11 @@
 <script>
+   import { popoverTooltip } from '#runtime/svelte/action/dom/tooltip';
+
    export let label = 'Demo'; // This prop is set in `HeaderButtonApplication` to 'SC'.
 </script>
 
 <!-- A simple text element / demo -->
-<span>{label}</span>
+<span use:popoverTooltip={'Svelte Component'}>{label}</span>
 
 <style>
    span {
