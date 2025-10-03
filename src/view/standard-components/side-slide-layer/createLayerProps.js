a79b248a7f2af5f928ee1ce222cb492fa8e56feb
typhonrt <support@typhonjs.io>
2025-10-02 23:16:03 -0700

    Additional side slide demo data.

--- src/view/standard-components/side-slide-layer/createLayerProps.js
+++ src/view/standard-components/side-slide-layer/createLayerProps.js
@@ -60,18 +60,17 @@ export function createLayerProps({ relative = false, sideAbs = true } = {})
          {
             icon: 'fas fa-crosshairs-simple',      // Font awesome icon _or_ a Svelte configuration object.
             svelte: { class: CurrentTargetPanel }, // A Svelte configuration object for the panel.
-            tooltip: 'Current Targets',            // Optional title / tooltip.
-            // tooltipDirection: 'RIGHT'              // You may provide the Foundry tooltip manager direction.
+            tooltip: 'Current Targets'             // Optional title / tooltip.
          },
          {
             condition: () => game.user.isGM,       // You may provide a function whether to display the item.
             icon: { class: DynamicIcon },          // Loads a Svelte component as item icon.
             svelte: { class: DummyPanel },
-            tooltip: 'Dummy Panel',
-            // tooltipDirection: 'RIGHT'              // You may provide the Foundry tooltip manager direction.
+            tooltip: 'Dummy Panel'
          }
       ],
 
-      // tooltips: false      // You may turn off all tooltips.
+      // tooltips: false,              // You may turn off all tooltips.
+      // tooltipDirection: 'RIGHT'     // You may provide the Foundry tooltip manager direction.
    };
 }
