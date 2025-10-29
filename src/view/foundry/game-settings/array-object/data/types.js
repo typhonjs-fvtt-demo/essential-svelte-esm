0d043745ca75d5ae1a012f6a6129671e367e5449
typhonrt <support@typhonjs.io>
2025-10-29 05:13:52 -0700

    Update #arrayObjectContext reference.

---
+++ src/view/foundry/game-settings/array-object/data/types.js
@@ -0,0 +1,9 @@
+/**
+ * This file is referenced by the imports field in `package.json` for `#arrayObjectContext`.
+ *
+ * It allows import types such as `@type {import('#arrayObjectContext').ItemEntryStore}` or
+ * `@type {import('#arrayObjectContext').ItemContext}` to resolve.
+ */
+
+export * from './ItemArrayObjectStore.js';
+export * from './ItemContext.js';
