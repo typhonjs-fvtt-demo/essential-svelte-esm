e84bf62aebeaf19a8c310e877c5032a008f50458
typhonrt <support@typhonjs.io>
2025-11-05 22:34:31 -0800

    Comments.

--- src/view/foundry/game-settings/array-object/data/types.js
+++ src/view/foundry/game-settings/array-object/data/types.js
@@ -3,6 +3,9 @@
  *
  * It allows import types such as `@type {import('#arrayObjectContext').ItemEntryStore}` or
  * `@type {import('#arrayObjectContext').ItemContext}` to resolve.
+ *
+ * This is just one way to type via JSDoc. Also see the side slide layer demo on how to use a separate TypeScript file
+ * to type your Svelte components with JSDoc: `src/view/standard-components/side-slide-layer/types.ts`.
  */
 
 export * from './ItemArrayObjectStore.js';
