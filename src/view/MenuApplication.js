139b966719375e95e3d817e225de3a55f865b3e8
typhonrt <support@typhonjs.io>
2026-06-18 14:21:58 -0700

    Remove demo code.

--- src/view/MenuApplication.js
+++ src/view/MenuApplication.js
@@ -67,13 +67,7 @@ export class MenuApplication extends SvelteApp
             scope: 'user',
             config: false,
             default: {},
-            type: Object,
-            onChange: (value, _, userId) =>
-            {
-               console.log(`!!! MenuApplication - onChange appStateMenuUser - change userId: ${userId}; game.user.id: ${game.userId}; wrong user: ${userId !== game.userId}`);
-               console.log(`!!! MenuApplication - onChange appStateMenuUser - change value: `, value?.position);
-               console.log(`!!! MenuApplication - onChange appStateMenuUser - stored value: `, game.settings.get(constants.moduleId, settings.appStateMenuUser)?.position);
-            }
+            type: Object
          }
       });
 
