74ed8e7b42134f9150b1d9e8bd4bc79331b3e4a6
typhonrt <support@typhonjs.io>
2026-07-21 16:15:31 -0700

    Update config.

---
+++ svelte.config.js
@@ -0,0 +1,10 @@
+// `svelte.config.js` is required for VSCode.
+
+/** @import {Options} from '@sveltejs/vite-plugin-svelte' */
+
+/** @type {Options} */
+const config = {
+   // Add your Svelte options or preprocessors here if needed.
+};
+
+export default config;
